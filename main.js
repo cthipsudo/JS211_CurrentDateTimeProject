2dc4f382d11ad96fdad97a6cd7d2b7221ffb9330
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2020-05-07 14:58:45 -0500

    inital commit, completed part one

--- main.js
+++ main.js
@@ -16,11 +16,25 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
+let someNumber = 4;
 
+let someConvertedStringNumber = someNumber.toString();
+
+//console.log("Hi");
+
+if (someConvertedStringNumber !== someNumber) {
+  console.log("You converted the string!");
+}
 
 // Write a JavaScript program to convert a string to the number.
 
+let someString = "6";
 
+let someConvertedNumberString = parseInt(someString);
+
+if(someConvertedNumberString === 6){
+  console.log("You converted the string!");
+}
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -30,21 +44,68 @@ const displayDate = () => {
   // * NaN
   // * String
   
+let testValue;
+
+const typeTeller = (someValue) => {
+  return typeof(someValue);
+}
+
+typeTeller(testValue);
 
   
 // Write a JavaScript program that adds 2 numbers together.
 
+let numOne = 4;
+let numTwo = 5;
 
+const numberAdd = (inputOne, inputTwo) => {
+  return inputOne + inputTwo;
+}
+
+numberAdd(numOne, numTwo);
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+let factorOne = -1;
+let factorTwo = 3;
 
+const numberEvaluator = (inputOne, inputTwo) => {
+  if (inputOne < 5 && inputTwo < 5){
+    console.log("This program runs");
+  }
+}
+
+numberEvaluator(factorOne, factorTwo);
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+// Write a program that runs 1 of the 2 are true.
 
+let factorThree = 1;
+let factorFour = 9;
+
+const numberEvaluatorTwo = (inputOne, inputTwo) => {
+  if (inputOne < 5 || inputTwo < 5){
+    console.log("This program runs");
+  }
+}
+
+numberEvaluatorTwo(factorThree, factorFour);
 
 // Write a JavaScript program that runs when both things are not true.  
+
+let factorFive = 0;
+let factorSix = 10;
+
+const numberEvaluatorThree = (inputOne, inputTwo) => {
+  if(inputOne === 4 && inputTwo === 6){
+    console.log("Hey those inputs are true, what the heck?");
+  } else{
+    console.log("Hey, both conditionals are not true!");
+  }
+}
+
+numberEvaluatorThree(factorFive, factorSix);
 
 // ***************************
 //         PART TWO
